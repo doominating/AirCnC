dd7ffad4d8db1adbc4996eb1c8d3160e3f1852a6
Afonso Amaro <doominating@gmail.com>
2020-02-07 01:42:57 -0300

    add create user session

---
+++ backend/src/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.store);
+
+module.exports = routes;
---
+++ backend/src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const dotenv = require('dotenv');
+
+const routes = require('./routes');
+
+const app = express();
+const server = http.Server(app);
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+const port = process.env.PORT || 3333;
+
+app.listen(port);
